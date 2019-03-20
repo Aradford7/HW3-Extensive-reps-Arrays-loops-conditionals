f0a6df6145f6408aedafb6a42deb8b94bca253bd
Ashley Radford <aradford7@outlook.com>
2019-03-20 16:44:22 -0700

    Commit - 2 Get Even

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log('JS is Running Running Running....')
+
+//EASY GOING
+//1. Write a loop that will log the numbers1 through 20.
+
+/*for(let i=1; i<=20; i++){
+    console.log('The number is' + i);}*/
+//"Commit 1 - Easy Going answered"
+
+//GET EVEN
+//2. Write a for loop that will log only the even numbers 
+//in 0 through 200.
+
+/*for(i = 0; i < 200; i++){
+    if ((i % 2) == 0){
+        console.log('This number is' + i)
+    }
+} */
+//"Commit 2 - Get Even answered"
+
+//EXCITED KITTEN
+//3.Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+//4.For every even number in your loop, log "...human...why 
+//you taking pictures of me?...", "...the catnip made me do it...",
+// or "...why does the red dot always get away..." at random.
+
+//Hint: You will need to use Math.random()
+
+for (let i = 0; i < 20; i++){
+    console.log('Love me, pet me! HSSSSSS!')
+}
+    if (Math.random ("...human...why you taking pictures of me?..."||"...the catnip made me do it..."||"...why does the red dot always get away..."));
+        console.log(Math.random())
+
